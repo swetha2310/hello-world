8f3794cc4173cd87c0b37d15ddab2959112aaf07
swetha2310 <89144008+swetha2310@users.noreply.github.com>
2023-03-06 16:15:52 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+ region = "ap-south-1"
+}
+resource "aws_instance" "myFirstinstance"{
+ ami = "ami-09ba48996007c8b50"
+ count = 1
+ key_name ="jenkins"
+ instance_type = "t2.small"
+ security_groups = ["security_jenkins_port"]
+ tags = {
+  Name = "jenkins_instance"
+ }
+}
+
+resource "aws_security_group" "security_jenkins_port" {
+  name        = "security_jenkins_port"
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound from Jenkins server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "security_jenkins_port"
+  }
+}
